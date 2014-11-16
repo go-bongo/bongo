12228e70b64cad973d4846d90bbedea13416c1f0
Jason Raede <jason@maxwellhealth.com>
2014-11-15 19:06:59 -0500

    Document with id + save

--- document.go
+++ document.go
@@ -7,48 +7,33 @@ import (
 	"github.com/oleiade/reflections"
 	"fmt"
 )
-type Collection struct {
-
-	// EncryptedFields []string
+type Document struct {
+	Id bson.ObjectId
+	Model interface{}
+	Connection *MongoConnection
 	Collection *mgo.Collection
 }
 
 var key = []byte("asdf1234asdf1234")
 
 
-// Save a model struct. Struct must have "Id" property. If that property is not a valid ObjectId then a new one is created.
-func (c *Collection) Save(mod interface{}) (error) {
+// Save a document
+func (d *Document) Save() (error) {
 
-	// 1) Make sure mod has an Id field
-	has, _ := reflections.HasField(mod, "Id")
-	if !has {
-		panic("Failed to save - model must have Id field")
-	}
+	// 1) If there's no ID, create a new one
 
-	// 2) If there's no ID, create a new one
 	
-	f, err := reflections.GetField(mod, "Id")
-	id := f.(bson.ObjectId)
-
-	if err != nil {
-		panic(err)
-	}
-	
-	if !id.Valid() {
+	if !d.Id.Valid() {
 		fmt.Println("Creating new ID")
-		id := bson.NewObjectId()
-		err := reflections.SetField(mod, "Id", id)  // err != nil
-
-		if err != nil {
-			panic(err)
-		}
+		d.Id := bson.NewObjectId()
 	}
 	
 
 
 	// 2) Convert the model into a map using the crypt library
-	modelMap := crypt.EncryptDocument(key, mod)
-	err =  c.Collection.Insert(modelMap)
+	modelMap := crypt.EncryptDocument(key, d.Model)
+	modelMap["_id"] = d.Id
+	err =  d.Collection.Insert(modelMap)
 
 	return err
 }
